24293de668a7c14561a92279d058f96e263fcbd5
CHugoJoh <hugjoh@chalmers.se>
2023-11-27 11:21:51 +0100

    pilly

--- src/CarDrawHelper.java
+++ src/CarDrawHelper.java
@@ -1,6 +1,10 @@
 import java.awt.*;
+import java.awt.image.BufferedImage;
 
 public class CarDrawHelper {
     Point carPoint = new Point();
 
+    BufferedImage =
+
+    Dictionary<Car, Image> dictImage = new Hashtable<>!();
 }
