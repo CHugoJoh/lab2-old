6c9c4edeebce164fb018ea690e74b1dfeab81c25
William Lindgren <william.lindgren19@gmail.com>
2023-11-27 11:10:07 +0100

    Update CarController.java

--- src/CarController.java
+++ src/CarController.java
@@ -33,6 +33,12 @@ public class CarController{
         CarController cc = new CarController();
 
         cc.cars.add(new Volvo240());
+        var saab = new Saab95();
+        saab.y = 100;
+        cc.cars.add(saab);
+        var scania = new Scandia();
+        scania.y = 200;
+        cc.cars.add(scania);
 
         // Start a new view and send a reference of self
         cc.frame = new CarView("CarSim 1.0", cc);
