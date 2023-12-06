ef2d414d8230869ac4ed24d7507559d1005195e6
Unlucky-e <emil@cederqvist.nu>
2023-12-06 15:33:25 +0100

    Update CarController.java

--- src/CarController.java
+++ src/CarController.java
@@ -1,8 +1,4 @@
-import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.util.ArrayList;
-import java.util.Iterator;
 
 /*
 * This class represents the Controller part in the MVC pattern.
